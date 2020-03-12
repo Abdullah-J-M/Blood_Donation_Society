a67e3e7e1643cfdf2b866342c9a27ce150af5dba
Abdullah J.M <abdullah.jamal@devsinc.com>
2020-03-12 18:48:14 +0500

    Defining basic functionality for Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,52 @@
 # Post Controller
 class PostsController < ApplicationController
-  def new; end
+  before_action :find_post, only: %i[show edit update destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save!
+      flash[:notice] = 'Post was successfully created'
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = 'Post was successfully updated'
+      redirect_to @post
+    else
+      flash[:alert] = 'Post was not updated'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 end
