8b7d063779412cfe0bbc69190e8f2fe5b14a29c6
Abdullah J.M <abdullah.jamal@devsinc.com>
2020-03-15 04:56:52 +0500

    Adding details to Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_114037) do
+ActiveRecord::Schema.define(version: 2020_03_14_234003) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "telephone"
+    t.string "requester"
+    t.string "bloodtype"
   end
 
 end
