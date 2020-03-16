0b599f295eb89240d3e336035fbd97a670011150
Abdullah J.M <abdullah.jamal@devsinc.com>
2020-03-16 18:59:10 +0500

    Fixing push requests error

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,7 @@
 module PostsHelper
+  def bloodytype_select
+    bt = [['AB+', 'AB+'], ['AB-', 'AB-'], ['A+', 'A+'], ['A-', 'A-'],
+          ['B+', 'B+'], ['B-', 'B-'], ['O+', 'O+'], ['O-', 'O-']]
+    bt
+  end
 end
